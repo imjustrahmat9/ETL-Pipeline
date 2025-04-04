a8744975a22fc9309f9c6c3b573e12323c31ccd2
RAHMAT HIDAYAT <mc013d5y1559@student.devacademy.id>
2025-04-04 15:40:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
+# Import fungsi-fungsi yang dibutuhkan dari modul internal
 from utils.extract import extract_all_products
 from utils.transform import full_cleaning_pipeline
 from utils.load import save_to_csv, save_to_existing_google_sheet, save_to_postgresql
+
+# Mengimpor pustaka dotenv dan os untuk mengelola variabel lingkungan
 from dotenv import load_dotenv
 import os
 
+# Memuat variabel lingkungan dari file .env
 load_dotenv()
 
-raw_data = extract_all_products("https://fashion-studio.dicoding.dev", max_pages=50)
-cleaned_json = full_cleaning_pipeline(raw_data)
+# Mengambil data produk mentah dari situs Fashion Studio dengan batas maksimal 50 halaman
+data_mentah = extract_all_products("https://fashion-studio.dicoding.dev", max_pages=50)
 
-save_to_csv(cleaned_json)
-spreadsheet_id = os.getenv("SHEETS_ID")
-save_to_existing_google_sheet(cleaned_json, spreadsheet_id)
-save_to_postgresql(cleaned_json)
+# Melakukan pembersihan dan transformasi data melalui pipeline pembersihan menyeluruh
+data_bersih = full_cleaning_pipeline(data_mentah)
+
+# Menyimpan data bersih ke dalam file CSV
+save_to_csv(data_bersih)
+
+# Mengambil ID spreadsheet dari variabel lingkungan
+id_spreadsheet = os.getenv("SHEETS_ID")
+
+# Menyimpan data ke Google Sheet yang sudah ada
+save_to_existing_google_sheet(data_bersih, id_spreadsheet)
+
+# Menyimpan data ke basis data PostgreSQL
+save_to_postgresql(data_bersih)
